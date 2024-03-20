f73d256723e6684c7e80afe2df615ba269e51306
supsup_hae <zksdbstjq@gmail.com>
2024-03-20 18:40:33 +0900

    :recycle: refactor: 코드 리팩토링

--- src/main/java/com/cotato/networking1/service/ExcelManageService.java
+++ src/main/java/com/cotato/networking1/service/ExcelManageService.java
@@ -4,5 +4,5 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface ExcelManageService {
 
-  public void upload(MultipartFile file);
+  void upload(MultipartFile file);
 }
--- src/main/java/com/cotato/networking1/service/PropertyService.java
+++ src/main/java/com/cotato/networking1/service/PropertyService.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public interface PropertyService {
 
-  public List<PropertyDto> findPropertyByZipCode(int zipCode); // 우편번호에 해당하는 모든 매물 정보
+  List<PropertyDto> findPropertyByZipCode(int zipCode); // 우편번호에 해당하는 모든 매물 정보
 
-  public Long enrollProperty(PropertyDto propertyDto); // 매물등록
+  Long enrollProperty(PropertyDto propertyDto); // 매물등록
 
-  public void deletePropertyByRoadNameAddress(String roadNameAddress); // 도로명주소에 해당하는 매물 삭제
+  void deletePropertyByRoadNameAddress(String roadNameAddress); // 도로명주소에 해당하는 매물 삭제
 
 }
