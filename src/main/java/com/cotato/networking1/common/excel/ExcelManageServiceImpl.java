2ad5c0791aa4916b685d90d66f9553391bf87876
hl3470 <hl3470@naver.com>
2024-03-20 20:25:33 +0900

    refactor: ExcelManageServiceImpl에 주석 추가

--- src/main/java/com/cotato/networking1/common/excel/ExcelManageServiceImpl.java
+++ src/main/java/com/cotato/networking1/common/excel/ExcelManageServiceImpl.java
@@ -39,29 +39,30 @@ public class ExcelManageServiceImpl {
 			int rows = sheet.getPhysicalNumberOfRows();
 
 			for (rowindex = 1; rowindex <= rows; rowindex++) {
-				//행을읽는다
 				XSSFRow row = sheet.getRow(rowindex);
-				if (row != null) {
-					String zipCode = "";
-					StringBuilder roadNameAddress = new StringBuilder();
-					StringBuilder landLotNameAddress = new StringBuilder();
 
+				String zipCode = "";
+				StringBuilder roadNameAddress = new StringBuilder();
+				StringBuilder landLotNameAddress = new StringBuilder();
+				if (row != null) {
 					//셀의 수
-					int cells = row.getPhysicalNumberOfCells();
-					for (columnindex = 0; columnindex < cells; columnindex++) {
+					int cellNumber = row.getPhysicalNumberOfCells();
+
+					for (columnindex = 0; columnindex < cellNumber; columnindex++) {
 						//셀값을 읽는다
 						XSSFCell cell = row.getCell(columnindex);
 						String value = readCellByType(cell);
-						log.info("value: " + value);
 
 						if (value == null && columnindex != 5) {
 							return DataResponse.error(HttpStatus.BAD_REQUEST.value());
 						}
 
+						//우편번호 만들기
 						if (columnindex == 0) {
 							zipCode = value;
 						}
 
+						//도로명 받기 만들기
 						if (columnindex == 1 || columnindex == 2 || columnindex == 3) {
 							roadNameAddress.append(value).append(" ");
 						} else if (columnindex == 4) {
@@ -73,6 +74,7 @@ public class ExcelManageServiceImpl {
 							roadNameAddress.append(value);
 						}
 
+						//지번주소 만들기
 						if (columnindex == 1 || columnindex == 2 || columnindex == 6) {
 							landLotNameAddress.append(value).append(" ");
 						} else if (columnindex == 7) {
@@ -81,23 +83,19 @@ public class ExcelManageServiceImpl {
 							landLotNameAddress.append(value);
 						}
 					}
-					log.info("zipCode: " + zipCode);
-					log.info("road: " + roadNameAddress.toString());
-					log.info("land: " + landLotNameAddress.toString());
-					estateService.saveEstate(zipCode, roadNameAddress.toString(), landLotNameAddress.toString());
-
 				}
+				estateService.saveEstate(zipCode, roadNameAddress.toString(), landLotNameAddress.toString());
 			}
 
 		} catch (Exception e) {
-			e.printStackTrace();
+			return DataResponse.error(HttpStatus.INTERNAL_SERVER_ERROR.value());
 		}
 		return DataResponse.ok();
 	}
 
+	//타입별로 내용 읽기
 	private String readCellByType(XSSFCell cell) {
 		if (cell != null) {
-			//타입별로 내용 읽기
 			switch (cell.getCellType()) {
 				case FORMULA:
 					return cell.getCellFormula();
