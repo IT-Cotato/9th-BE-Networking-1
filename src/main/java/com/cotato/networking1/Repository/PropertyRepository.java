6091be99ec6714dbc0d8c872798d0d6954b95196
woogieon8on <swp0927@naver.com>
2024-03-26 02:40:38 +0900

    Feat: add PropertyRepository

---
+++ src/main/java/com/cotato/networking1/Repository/PropertyRepository.java
@@ -0,0 +1,7 @@
+package com.cotato.networking1.Repository;
+
+import com.cotato.networking1.Entity.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PropertyRepository extends JpaRepository<Property, Long> {
+}
