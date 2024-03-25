bf95a248373006350594a4c33658c2113a93d81f
youth <boysoeng@naver.com>
2024-03-25 16:57:31 +0900

    feat: 사용자가 매물을 등록하는 API 구현
    - 우편번호, 도로명 주소, 지번 주소로 등록
    - 생성된 id 반환
    - null값이 등록되면 400에러 반환

--- src/main/java/com/cotato/networking1/api/controller/PropertyController.java
+++ src/main/java/com/cotato/networking1/api/controller/PropertyController.java
@@ -1,10 +1,17 @@
 package com.cotato.networking1.api.controller;
 
+import com.cotato.networking1.api.dto.CreatePropertyRequest;
+import com.cotato.networking1.api.dto.CreatePropertyResponse;
 import com.cotato.networking1.api.dto.FindPropertiesResponse;
 import com.cotato.networking1.domain.service.PropertyService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +26,10 @@ public class PropertyController {
     @GetMapping("/properties")
     public ResponseEntity<FindPropertiesResponse> findProperty(@RequestParam("zip-code") String zipCode){
         return ResponseEntity.ok(propertyService.findProperties(zipCode));
+    }
+
+    @PostMapping("/properties")
+    public ResponseEntity<CreatePropertyResponse> createProperty(@RequestBody @Valid CreatePropertyRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(propertyService.createProperty(request));
     }
 }
--- src/main/java/com/cotato/networking1/domain/service/PropertyService.java
+++ src/main/java/com/cotato/networking1/domain/service/PropertyService.java
@@ -1,7 +1,10 @@
 package com.cotato.networking1.domain.service;
 
+import com.cotato.networking1.api.dto.CreatePropertyRequest;
+import com.cotato.networking1.api.dto.CreatePropertyResponse;
 import com.cotato.networking1.api.dto.FindPropertiesResponse;
 import com.cotato.networking1.api.dto.PropertyResponse;
+import com.cotato.networking1.domain.entity.Property;
 import com.cotato.networking1.domain.repository.PropertyRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -20,5 +23,13 @@ public class PropertyService {
                 .map(PropertyResponse::from)
                 .toList();
         return FindPropertiesResponse.from(responses);
+    }
+
+    @Transactional
+    public CreatePropertyResponse createProperty(CreatePropertyRequest request) {
+        Property createdProperty = Property.of(request.zipCode(), request.roadNameAddress(),
+                request.landLotNameAddress());
+        propertyRepository.save(createdProperty);
+        return new CreatePropertyResponse(createdProperty.getId());
     }
 }
