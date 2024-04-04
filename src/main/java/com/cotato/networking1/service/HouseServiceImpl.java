cc8f5cc98ce184dc51075e09319d968cf9b0b471
leejunnyeop <daffodil8620@naver.com>
2024-04-04 21:07:22 +0900

    Refactor : findBy메소드 util 캡슐화

---
+++ src/main/java/com/cotato/networking1/service/HouseServiceImpl.java
@@ -0,0 +1,2 @@
+package com.cotato.networking1.service;public class HouseImpl {
+}
---
+++ src/main/java/com/cotato/networking1/utility/FindByUtil.java
@@ -0,0 +1,18 @@
+package com.cotato.networking1.utility;
+
+import com.cotato.networking1.enity.HouseDao;
+import com.cotato.networking1.repository.HouseRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class FindByUtil {
+
+    private final HouseRepository houseRepository;
+
+    public HouseDao existentByHouseId(Long id){
+        HouseDao houseFindById = houseRepository.findById(id).orElseThrow(
+                ()-> new IllegalArgumentException("없는 매물 입니다"));
+        return houseFindById;
+    }
+
+}
